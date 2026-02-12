496d43656209fed6b35871d203cd72a16ec23e1f
khushi <khushi.sysquare@gmail.com>
2026-02-12 18:26:18 +0530

    Pytest updated

---
+++ Day1/test_pytest_try.py
@@ -0,0 +1,14 @@
+import pytest
+
+class Test_login:
+    def test_login(self):
+        print("This is login test by email")
+        assert True
+
+    def test_logout(self):
+        print("This is logout test")
+        assert True
+
+    def test_phone(self):
+        print("This is login by phone number")
+        assert True
